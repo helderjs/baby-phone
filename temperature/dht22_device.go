46dafea05e827a7b69d63133d753d7ed347a2f12
Helder Santana <contato@heldersantana.net>
2019-08-27 23:37:11 +0200

    Fix sensorType Parameter

--- temperature/dht22_device.go
+++ temperature/dht22_device.go
@@ -5,7 +5,7 @@ import "github.com/d2r2/go-dht"
 type dht22Device struct{}
 
 func (d *dht22Device) ReadTemperature() float32 {
-	temperature, _, _, _ := dht.ReadDHTxxWithRetry(dht.DHT11, 4, false, 10)
+	temperature, _, _, _ := dht.ReadDHTxxWithRetry(dht.DHT22, 4, false, 10)
 
 	return temperature
 }
